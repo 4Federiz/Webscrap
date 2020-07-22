6a0c3109f4ada9327a3cefad8fcdf5f1bf8d48b3
Hassan Asqiriba <federiz@msn.com>
2020-07-22 14:41:51 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Webscrap
+
+Simple web scraping tool now for Flutter
+
+![Demo Image 01](asset/screenshot.png)
---
+++ android/app/src/main/kotlin/com/federiz/webscrap/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.federiz.webscrap
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
